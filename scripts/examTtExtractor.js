e3e1c4ea12b134fd343a29fe20c7fab5502b7d1b
jeremiah-ang <jeremiahang94@gmail.com>
2019-04-25 11:50:49 +0800

    add regex support for "by time" desktop version

--- scripts/examTtExtractor.js
+++ scripts/examTtExtractor.js
@@ -1,9 +1,11 @@
-const regex = /((.+)\n.+\n.+\n.+\n([0-9]{2})-([0-9]{2})-.+\n([0-9]{1,2}):([0-9]{2}).+\n.+\n(.+))/gm;
-const regexMobile = /(Class\s*(.+)\s.+\s*.+\s*.+\s*.+\s*.+\s*Exam Date\s*([0-9]{2})-([0-9]{2}).+\s*Schedule\s*([0-9]{1,2}):([0-9]{2}).+\s*.+\s*.+\s*Exam Venue\s*(.+)\s*)/gm;
+const regex = /((?<code>.+)\n.+\n.+\n.+\n(?<date>[0-9]{2})-(?<month>[0-9]{2})-.+\n(?<hour>[0-9]{1,2}):(?<minute>[0-9]{2}).+\n.+\n(?<venue>.+))/gm;
+const regexMobile = /(Class\s*(?<code>.+)\s.+\s*.+\s*.+\s*.+\s*.+\s*Exam Date\s*(?<date>[0-9]{2})-(?<month>[0-9]{2}).+\s*Schedule\s*(?<hour>[0-9]{1,2}):(?<minute>[0-9]{2}).+\s*.+\s*.+\s*Exam Venue\s*(?<venue>.+)\s*)/gm;
+const regexByDate = /[A-Z]+ (?<month>[A-Z]{3}).*?(?<date>[0-9]{1,2})\s+(?<hour>[0-9]{1,2}):(?<minute>[0-9]{2}).+\s+(?<code>.+)\s+.+\s+VENUE:\s+(?<venue>.+)/gm;
 
 const months = ["INVALID", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
 
 function extractExamTtMonth(month) {
+    if (months.includes(month)) return month;
     return months[parseInt(month)];
 }
 
@@ -15,7 +17,13 @@ function extractExamTtHour(hour) {
         : (hourNum + 12) + "";
 }
 
+function extractExamTtDate(date) {
+    const dateNum = parseInt(date);
+    return (date < 10) ? "0" + date : "" + date;
+}
+
 function extractExamTtModulesRegex(str, reg) {
+    console.log(str);
     let modules = [], m;
     while ((m = reg.exec(str)) !== null) {
         // This is necessary to avoid infinite loops with zero-width matches
@@ -23,13 +31,14 @@ function extractExamTtModulesRegex(str, reg) {
             reg.lastIndex++;
         }
         
+        m = m.groups;
         const module = {
-            code: m[2],
-            date: m[3],
-            month: extractExamTtMonth(m[4]),
-            hour: extractExamTtHour(m[5]),
-            minute: m[6],
-            location: m[7]
+            code: m['code'],
+            date: extractExamTtDate(m['date']),
+            month: extractExamTtMonth(m['month']),
+            hour: extractExamTtHour(m['hour']),
+            minute: m['minute'],
+            location: m['venue']
         }
         modules.push(module)
     }
@@ -44,9 +53,16 @@ function extractExamTtModulesMobile(str) {
     return extractExamTtModulesRegex(str, regexMobile);
 }
 
+function extractExamTtModulesByDateDesktop(str) {
+    return extractExamTtModulesRegex(str.toUpperCase(), regexByDate);
+}
+
 function extractExamTtModules(str) {
-    const modules = extractExamTtModulesDesktop(str);
+    let modules = extractExamTtModulesDesktop(str);
     if (modules.length > 0) return modules;
-    return extractExamTtModulesMobile(str);
+    modules = extractExamTtModulesMobile(str);
+    if (modules.length > 0) return modules;
+    modules = extractExamTtModulesByDateDesktop(str);
+    return modules;
     
 }
