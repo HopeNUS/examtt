61b1a490016bc2cddb2ba51cbc20a3eb7727ddbe
jeremiah-ang <jeremiahang94@gmail.com>
2019-11-18 00:43:37 +0800

    Add wake server script

--- scripts/apiUrls.js
+++ scripts/apiUrls.js
@@ -1,15 +1,16 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://hopenus-examtt-backend.herokuapp.com";
+$.get(BASE_URL);
 
-const LIFEGROUP_EXT = "/lifegroup"
-const LIFEGROUP_URL = BASE_URL + LIFEGROUP_EXT
+const LIFEGROUP_EXT = "/lifegroup";
+const LIFEGROUP_URL = BASE_URL + LIFEGROUP_EXT;
 function getLifegroupUrl() { return LIFEGROUP_URL; }
 
-const BATCH_INSERT_EXAMS = BASE_URL + "/batch_insert_exams"
-const GET_EXAMS_URL = BASE_URL + '/get_exams'
+const BATCH_INSERT_EXAMS = BASE_URL + "/batch_insert_exams";
+const GET_EXAMS_URL = BASE_URL + '/get_exams';
 function getExamTtUrl() { return GET_EXAMS_URL; }
 function getAddExamTtUrl() { return BATCH_INSERT_EXAMS; }
 
-const WARRIOR_UNSUB_PRAYER_SLOT_URL =  BASE_URL + '/batch_delete_prayer_warriors'
-const WARRIOR_SUB_PRAYER_SLOT_URL = BASE_URL + '/batch_insert_prayer_warriors'
+const WARRIOR_UNSUB_PRAYER_SLOT_URL =  BASE_URL + '/batch_delete_prayer_warriors';
+const WARRIOR_SUB_PRAYER_SLOT_URL = BASE_URL + '/batch_insert_prayer_warriors';
 function getDeleteWarriorSubscriptionUrl() { return WARRIOR_UNSUB_PRAYER_SLOT_URL; }
 function getAddWarriorSubscriptionUrl() { return WARRIOR_SUB_PRAYER_SLOT_URL; }
